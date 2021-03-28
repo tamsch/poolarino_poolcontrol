3d1f45627686e58d42a20f53f13043e49c4afb17
Tammo <64162879+tamsch@users.noreply.github.com>
2021-03-28 19:35:06 +0200

    Update install_poolcontrol.sh

--- install_poolcontrol.sh
+++ install_poolcontrol.sh
@@ -17,61 +17,61 @@ _start=1
 
 _end=100
 
-curl -sL https://deb.nodesource.com/setup_12.x &> /dev/null | sudo -E bash - &> /dev/null
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 ProgressBar 3 ${_end}
-sudo apt install nodejs -y &> /dev/null
+sudo apt install nodejs -y
 ProgressBar 6 ${_end}
-sudo apt-get install gcc g++ make -y &> /dev/null
+sudo apt-get install gcc g++ make -y
 ProgressBar 10 ${_end}
-sudo timedatectl set-timezone Europe/Berlin &> /dev/null
+sudo timedatectl set-timezone Europe/Berlin
 ProgressBar 14 ${_end}
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - &> /dev/null
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 ProgressBar 17 ${_end}
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" &> /dev/null | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list &> /dev/null
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 ProgressBar 21 ${_end}
-sudo apt-get update &> /dev/null
+sudo apt-get update
 ProgressBar 25 ${_end}
-sudo apt-get install -y mongodb-org &> /dev/null
+sudo apt-get install -y mongodb-org
 ProgressBar 27 ${_end}
-sudo systemctl start mongod.service &> /dev/null
+sudo systemctl start mongod.service
 ProgressBar 30 ${_end}
-sudo systemctl enable mongod.service &> /dev/null
+sudo systemctl enable mongod.service
 ProgressBar 33 ${_end}
-sudo git clone https://github.com/tamsch/poolarino_poolcontrol.git /home/poolarino_poolcontrol &> /dev/null
+sudo git clone https://github.com/tamsch/poolarino_poolcontrol.git /home/poolarino_poolcontrol
 ProgressBar 36 ${_end}
-sudo rm -r /home/poolarino_poolcontrol/angular-src/ &> /dev/null
+sudo rm -r /home/poolarino_poolcontrol/angular-src/
 ProgressBar 39 ${_end}
-sudo npm --prefix /home/poolarino_poolcontrol/ install &> /dev/null
+sudo npm --prefix /home/poolarino_poolcontrol/ install
 ProgressBar 44 ${_end}
 sudo echo "db.createUser({user: 'pool', pwd: 'pool', roles: ['readWrite']})" > /home/mongodb.js
 ProgressBar 45 ${_end}
-sudo mongo pool /home/mongodb.js &> /dev/null
+sudo mongo pool /home/mongodb.js
 ProgressBar 47 ${_end}
-echo "module.exports = {database: 'mongodb://pool:pool@127.0.0.1:27017/pool' , secret:'deinscret'}" > /home/poolarino_poolcontrol/config/database.js &> /dev/null
+echo "module.exports = {database: 'mongodb://pool:pool@127.0.0.1:27017/pool' , secret:'deinscret'}" > /home/poolarino_poolcontrol/config/database.js
 ProgressBar 51 ${_end}
-sudo npm install -g pm2 &> /dev/null
+sudo npm install -g pm2
 ProgressBar 54 ${_end}
-sudo pm2 startup &> /dev/null
+sudo pm2 startup
 ProgressBar 57 ${_end}
-sudo pm2 start /home/poolarino_poolcontrol/app.js &> /dev/null
+sudo pm2 start /home/poolarino_poolcontrol/app.js
 ProgressBar 61 ${_end}
-sudo apt-get install -y lighttpd &> /dev/null
+sudo apt-get install -y lighttpd
 ProgressBar 66 ${_end}
-sudo apt-get install pure-ftpd -y &> /dev/null
+sudo apt-get install pure-ftpd -y
 ProgressBar 70 ${_end}
-sudo groupadd ftpgroup &> /dev/null
+sudo groupadd ftpgroup
 ProgressBar 73 ${_end}
-sudo useradd ftpuser -g ftpgroup -s /sbin/nologin -d &> /dev/null
+sudo useradd ftpuser -g ftpgroup -s /sbin/nologin -d
 ProgressBar 78 ${_end}
-sudo chown -R ftpuser:ftpgroup /var/www/html &> /dev/null
+sudo chown -R ftpuser:ftpgroup /var/www/html
 ProgressBar 84 ${_end}
-(echo poolarino; echo poolarino) | sudo pure-pw useradd uploadeng -u ftpuser -g ftpgroup -d /var/www/html -m &> /dev/null
+(echo poolarino; echo poolarino) | sudo pure-pw useradd uploadeng -u ftpuser -g ftpgroup -d /var/www/html -m
 ProgressBar 87 ${_end}
-sudo pure-pw mkdb &> /dev/null
+sudo pure-pw mkdb
 ProgressBar 91 ${_end}
-sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/60puredb &> /dev/null
+sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/60puredb
 ProgressBar 95 ${_end}
-sudo service pure-ftpd restart &> /dev/null
+sudo service pure-ftpd restart
 ProgressBar 100 ${_end}
 
 printf '\nDie Poolarino-Poolsteuerung wurde erfolgreich installiert!\n'
