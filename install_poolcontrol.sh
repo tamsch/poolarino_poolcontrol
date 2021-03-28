5dddf91e88129e83c03909d24545a8184a2c045b
Tammo <64162879+tamsch@users.noreply.github.com>
2021-03-28 17:48:28 +0200

    Update install_poolcontrol.sh

--- install_poolcontrol.sh
+++ install_poolcontrol.sh
@@ -1,67 +1,79 @@
 #!/bin/bash
 echo "Starting Poolarino-Poolcontrol installation"
-echo "getting node repo..."
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - 2> /dev/null
-echo "installing nodejs..."
-sudo apt install nodejs -y 2> /dev/null
-echo "finished nodejs installation..."
-echo "installing build tools..."
-sudo apt-get install gcc g++ make -y 2> /dev/null
-echo "leaving root..."
-echo "setting timezone..."
-sudo timedatectl set-timezone Europe/Berlin 2> /dev/null
-echo "ttimezone set"
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - 2> /dev/null
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-echo "updating repos..."
-sudo apt-get update 2> /dev/null
-echo "installing mongodb"
-sudo apt-get install -y mongodb-org 2> /dev/null
-echo "asdfasfd"
-sudo systemctl start mongod.service 2> /dev/null
-echo "enabling mongodb (autostart)..."
-sudo systemctl enable mongod.service 2> /dev/null
-echo "starting installing poolcontrol..."
-cd ..
-echo "cloning repo..."
-sudo git clone https://github.com/tamsch/poolarino_poolcontrol.git 2> /dev/null
-echo "werwerwer"
-cd poolarino_poolcontrol
-echo "eiieie"
-echo "urueueue"
-cd ../home/poolarino_poolcontrol/
-echo "installing packages..."
-sudo npm i 2> /dev/null
-echo "werwerewrwrwer"
-echo "ghgfhfghfh"
-sudo rm -r angular-src/
-echo "creating mongodb database..."
-echo "db.createUser({user: 'pool', pwd: 'pool', roles: ['readWrite']})" > /tmp/mongodb.js
-echo "creating database file..."
-echo "module.exports = {database: 'mongodb://pool:pool@127.0.0.1:27017/pool' , secret:'deinscret'}" > ~/../poolarino_poolcontrol/config/database.js
-echo "adding processmanager..."
-sudo npm install -g pm2 2> /dev/null
-echo "jzjjzjj"
-sudo pm2 startup 2> /dev/null
-echo "rtrttzrtzrtrz"
-cd poolarino_poolcontrol/
-echo "uioiuoio"
-sudo pm2 start app.js
-echo "installing webserver..."
-sudo apt-get install -y lighttpd 2> /dev/null
-echo "installing ftp program..."
-sudo apt-get install pure-ftpd -y 2> /dev/null
-echo "uziuzuizuiz"
-sudo groupadd ftpgroup
-echo "errttrttrrttr"
-sudo useradd ftpuser -g ftpgroup -s /sbin/nologin -d /dev/null
-echo "hthhhhh"
-sudo chown -R ftpuser:ftpgroup /var/www/html
-echo "tzuzuzuzutztuz"
-(echo poolarino; echo poolarino) | sudo pure-pw useradd upload -u ftpuser -g ftpgroup -d /var/www/html -m
-sudo pure-pw mkdb
-echo "rtzrtztrrztrrztrtz"
-sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/60puredb
-echo "jzjjjjtzjzt"
-sudo service pure-ftpd restart
-echo "finished everything, poolcontrol ready"
+
+function ProgressBar {
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+printf "\rInstallationsfortschritt : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+
+}
+
+_start=1
+
+_end=100
+
+for number in $(seq ${_start} ${_end})
+do
+	
+	curl -sL https://deb.nodesource.com/setup_12.x &> /dev/null | sudo -E bash - &> /dev/null
+	ProgressBar ${3} ${_end}
+	sudo apt install nodejs -y &> /dev/null
+	ProgressBar ${6} ${_end}
+	sudo apt-get install gcc g++ make -y &> /dev/null
+	ProgressBar ${10} ${_end}
+	sudo timedatectl set-timezone Europe/Berlin &> /dev/null
+	ProgressBar ${14} ${_end}
+	wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - &> /dev/null
+	ProgressBar ${17} ${_end}
+	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" &> /dev/null | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list &> /dev/null
+	ProgressBar ${21} ${_end}
+	sudo apt-get update &> /dev/null
+	ProgressBar ${25} ${_end}
+	sudo apt-get install -y mongodb-org &> /dev/null
+	ProgressBar ${27} ${_end}
+	sudo systemctl start mongod.service &> /dev/null
+	ProgressBar ${30} ${_end}
+	sudo systemctl enable mongod.service &> /dev/null
+	ProgressBar ${33} ${_end}
+	sudo git clone https://github.com/tamsch/poolarino_poolcontrol.git /home/poolarino_poolcontrol &> /dev/null
+	ProgressBar ${36} ${_end}
+	sudo npm --prefix /home/poolarino_poolcontrol/ install &> /dev/null
+	ProgressBar ${39} ${_end}
+	sudo rm -r /home/poolarino_poolcontrol/angular-src/
+	ProgressBar ${44} ${_end}
+	echo "db.createUser({user: 'pool', pwd: 'pool', roles: ['readWrite']})" > /tmp/mongodb.js
+	ProgressBar ${47} ${_end}
+	echo "module.exports = {database: 'mongodb://pool:pool@127.0.0.1:27017/pool' , secret:'deinscret'}" > ~/../poolarino_poolcontrol/config/database.js
+	ProgressBar ${51} ${_end}
+	sudo npm install -g pm2 &> /dev/null
+	ProgressBar ${54} ${_end}
+	sudo pm2 startup &> /dev/null
+	ProgressBar ${57} ${_end}
+	sudo pm2 start /home/poolarino_poolcontrol/app.js
+	ProgressBar ${61} ${_end}
+	sudo apt-get install -y lighttpd &> /dev/null
+	ProgressBar ${66} ${_end}
+	sudo apt-get install pure-ftpd -y &> /dev/null
+	ProgressBar ${70} ${_end}
+	sudo groupadd ftpgroup
+	ProgressBar ${73} ${_end}
+	sudo useradd ftpuser -g ftpgroup -s /sbin/nologin -d /dev/null
+	ProgressBar ${78} ${_end}
+	sudo chown -R ftpuser:ftpgroup /var/www/html
+	ProgressBar ${84} ${_end}
+	(echo poolarino; echo poolarino) | sudo pure-pw useradd upload -u ftpuser -g ftpgroup -d /var/www/html -m
+	ProgressBar ${87} ${_end}
+	sudo pure-pw mkdb
+	ProgressBar ${91} ${_end}
+	sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/60puredb
+	ProgressBar ${95} ${_end}
+	sudo service pure-ftpd restart
+	ProgressBar ${100} ${_end}
+    
+done
+printf '\nDie Poolarino-Poolsteuerung wurde erfolgreich installiert!\n'
